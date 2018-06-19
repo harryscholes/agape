dab0821a0c65b5fe69e5aac74e274caf7ffddec4
harryscholes <harryscholes@gmail.com>
2018-06-19 12:48:41 +0100

    Add colors argument for plots with > 6 series

--- agape/plotting.py
+++ agape/plotting.py
@@ -3,12 +3,12 @@
 import os
 import numpy as np
 from typing import Dict
+import matplotlib as mpl
 
 try:
     assert 'DISPLAY' in os.environ
 except AssertionError:
-    import matplotlib
-    matplotlib.use('Agg')
+    mpl.use('Agg')
 finally:
     import matplotlib.pyplot as plt
     import seaborn as sns
@@ -17,7 +17,8 @@ __all__ = ['plot_loss']
 
 
 def plot_loss(plot_data: Dict[str, dict], filename: str,
-              end_epoch: int = None, log_y: bool = False):
+              end_epoch: int = None, log_y: bool = False,
+              colors=sns.color_palette('colorblind')):
     '''Plot the training and validation loss from Keras training histories.
 
     # Arguments
@@ -36,11 +37,10 @@ def plot_loss(plot_data: Dict[str, dict], filename: str,
                 np.array(history['loss'])[:n],
                 color=c, linestyle='--', alpha=0.5)
 
-    c = sns.color_palette('colorblind')
     fig, ax = plt.subplots(1, figsize=(4, 3))
 
     for idx, (label, history) in enumerate(plot_data.items()):
-        plotter(history, label, c[idx], end_epoch)
+        plotter(history, label, colors[idx], end_epoch)
 
     if log_y:
         ax.set_yscale("log", nonposy='clip')
