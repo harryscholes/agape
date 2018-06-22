3020af8916c4bb091f7e453e10acd4e8010f6d48
harryscholes <harryscholes@gmail.com>
2018-06-22 15:48:36 +0100

    Add performance of deepNF on MIPS CV

---
+++ agape/deepNF/performance.py
@@ -0,0 +1,17 @@
+# Approximate performance of deepNF on MIPS CV taken from paper Fig. 2.
+CV = {
+    'level1': {
+        'M_AUPR': 0.6,
+        'accuracy': 0.31,
+        'f1': 0.59,
+        'm_AUPR': 0.73},
+    'level2': {
+        'M_AUPR': 0.47,
+        'accuracy': 0.21,
+        'f1': 0.54,
+        'm_AUPR': 0.64},
+    'level3': {
+        'M_AUPR': 0.37,
+        'accuracy': 0.21,
+        'f1': 0.51,
+        'm_AUPR': 0.6}}
