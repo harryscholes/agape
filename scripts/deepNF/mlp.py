2b33e72994b22c664152260a05443db000dd3b38
harryscholes <harryscholes@gmail.com>
2018-08-06 13:13:54 +0100

    Add concatenation of all levels of annotation

--- scripts/deepNF/mlp.py
+++ scripts/deepNF/mlp.py
@@ -85,7 +85,7 @@ else:
 # Validation type
 validation_types = {
     'cv': ('P_1', 'P_2', 'P_3', 'F_1', 'F_2', 'F_3', 'C_1', 'C_2', 'C_3'),
-    'cerevisiae': ('level1', 'level2', 'level3')}
+    'cerevisiae': ('level1', 'level2', 'level3', 'all')}
 
 try:
     annotation_types = validation_types[validation]
@@ -122,7 +122,12 @@ def main():
     # Train classifier
     stdout('Running cross-validation for', level)
 
-    annotations = annotation_file[level].astype('int32')
+    if level == 'all':
+        annotations = np.hstack((annotation_file['level1'],
+                                 annotation_file['level2'],
+                                 annotation_file['level3'])).astype('int32')
+    else:
+        annotations = annotation_file[level].astype('int32')
 
     # Silence certain warning messages during cross-validation
     for w in (sklearn.exceptions.UndefinedMetricWarning, UserWarning,
